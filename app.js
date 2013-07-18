374ea60f0a70c52eca0cb3e23b0ddbcdd675c247
EchoFUN <xukai.ken@gmail.com>
2013-07-18 16:18:15 +0800

    basic frame.

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * @author XU Kai(xukai.ken@gmail.com) botobe.net
+ * @version 0.0.1
+ *
+ * 2013-07-18 初始化文件完成最基本的结构
+ * 用于完成NoF5的功能
+ */
+
+(function() {
+   
+   'use strict';
+
+   
+   
+})(); 
